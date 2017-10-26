ae94fc1cf1a08b52a84ff3c6d701c1fcd766f3d1
rodmcnew <rodmcnew@gmail.com>
2017-10-26 11:23:51 -0500

    update copywrite dates

--- src/Exception/AxosoftLoggerException.php
+++ src/Exception/AxosoftLoggerException.php
@@ -12,7 +12,7 @@ namespace Reliv\RcmAxosoft\Exception;
  * @category  Reliv
  * @package   Reliv\RcmAxosoft\Exception
  * @author    James Jervis <jjervis@relivinc.com>
- * @copyright 2014 Reliv International
+ * @copyright 2017 Reliv International
  * @license   License.txt New BSD License
  * @version   Release: <package_version>
  * @link      https://github.com/reliv
